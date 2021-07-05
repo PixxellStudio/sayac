b4d462c440d694ebff5c87925e65518f09107c5c
PixxellStudio <86629254+PixxellStudio@users.noreply.github.com>
2021-07-05 15:37:36 +0300

    sayac

---
+++ script.js
@@ -0,0 +1,24 @@
+const ekranEl = document.querySelector
+(".ekran");
+
+const artiButton = document.
+querySelector(".arti");
+
+const eksiButton = document.
+querySelector(".eksi");
+
+console.log(ekranEl, artiButton , eksiButton);
+
+let sayac = 0;
+
+artiButton.addEventListener("click" , () => {
+    sayac = sayac + 1;
+    console.log("artı tıklandı");
+    ekranEl.innerHTML = sayac;
+});
+
+eksiButton.addEventListener("click",() => {
+    sayac = sayac - 1;
+    console.log("eksi tıklandı"); 
+    ekranEl.innerHTML = sayac;
+});
